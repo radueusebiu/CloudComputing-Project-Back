72bb2efa5fc9bd724e93365622a3af1c949f7033
eusebiu <raduflorin18@stud.ase.ro>
2022-05-11 11:21:38 +0300

    Initial Backend Setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World again!')
+  });
+
+app.listen(port, () => {
+    console.log(`Cloud Computing app listen on port ${port}!`)
+});
